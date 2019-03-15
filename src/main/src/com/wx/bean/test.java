0eabe0c93e0530ea34a3c9a3a0576bb04a75f7e7
Administrator <970696023@qq.com>
2019-03-15 18:00:25 +0800

    page2

---
+++ src/main/src/com/wx/bean/test.java
@@ -0,0 +1,22 @@
+package com.wx.bean;
+
+public class test {
+    private String name;
+
+    public test(String name) {
+        this.name = name;
+    }
+
+    public test() {
+
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
